187957710453c380534397d3eeac59071d6dbf02
Netanel <ne.zaffran@gmail.com>
2021-07-08 22:25:30 +0300

    fix bug, remove last character if break line

--- server-api/src/tools/parser.ts
+++ server-api/src/tools/parser.ts
@@ -1,11 +1,22 @@
 export const parser = (data: string) => {
+  //split by row
   let allRows = data.split(/\r?\n|\r/);
+
+  //remove last character if break line
+  for (let i = allRows.length - 1; i >= 0; i--) {
+    if (/^\s*$/.test(allRows[i])) {
+      allRows.splice(allRows.length - 1);
+    } else {
+      break;
+    }
+  }
+  console.log(`we remove the last character of the folder because it was a break line`);
+
+  //split by col
   let RowDividedByCell = allRows.map((x) => x.split(/,|;/));
   let keyObj = RowDividedByCell[0];
 
-  let result = RowDividedByCell.filter(
-    (x, i) => i !== 0 && i < RowDividedByCell.length - 1
-  ).map((x, i) => {
+  let result = RowDividedByCell.filter((x, i) => i !== 0).map((x, i) => {
     let resultFinal = {};
 
     for (let j = 0; j < x.length; j++) {
